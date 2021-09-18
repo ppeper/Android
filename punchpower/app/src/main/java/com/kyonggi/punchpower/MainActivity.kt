7fded2cb9290c94a51fc0e46961400b2e4768ffa
Joonhoo <mnc5@naver.com>
2021-09-18 19:32:17 +0900

    210918

--- punchpower/app/src/main/java/com/kyonggi/punchpower/MainActivity.kt
+++ punchpower/app/src/main/java/com/kyonggi/punchpower/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.kyonggi.punchpower
 
+import android.animation.*
 import android.content.Context
 import android.content.Intent
 import android.hardware.Sensor
@@ -9,6 +10,8 @@ import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import com.kyonggi.punchpower.databinding.ActivityMainBinding
 import java.lang.Exception
 import kotlin.math.pow
@@ -43,6 +46,12 @@ class MainActivity : AppCompatActivity() {
                 }
                 // 측정이 시작된 경우
                 if (isStart) {
+                    // 애니메이션 제거
+                        binding.imageView.clearAnimation()
+                    // 5초간 최대값을 측정. 현재측정된 값이 지금까지 측정된 최대 값보다 크면 현재 값으로 변경
+                    if (maxPower < power) {
+                        maxPower = power
+                    }
                     // 최초 측정후 3초가 지났으면 측정을 끝낸다.
                     if (System.currentTimeMillis() - startTime > 3000) {
                         isStart = false
@@ -80,13 +89,54 @@ class MainActivity : AppCompatActivity() {
         maxPower = 0.0
         isStart = false
         startTime = 0L
-        binding.stateLabel.text = R.string.state_label.toString()
+        binding.stateLabel.text = "핸드폰을 손에쥐고 주먹을 내지르세요"
         // 센서의 변화 값을 처리할 리스너를 등록한다.
         sensorManager.registerListener(
             eventListener,
             sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION),
             SensorManager.SENSOR_DELAY_NORMAL
         )
+        // 에니메이션 추가
+//        binding.imageView.startAnimation(AnimationUtils.loadAnimation(this, R.anim.tran))
+//        with(binding) {
+//            val animation = AnimationUtils.loadAnimation(this@MainActivity, R.anim.alpha_scale)
+//            imageView.startAnimation(animation)
+//
+//            animation.setAnimationListener(object: Animation.AnimationListener {
+//                override fun onAnimationStart(animation: Animation?) {
+//                    // 애니메이션이 시작될때의 코드를 작성
+//                }
+//
+//                override fun onAnimationEnd(animation: Animation?) {
+//                    // 애니메이션이 종료될때의 코드를 작성
+//                }
+//
+//                override fun onAnimationRepeat(animation: Animation?) {
+//                    // 애니메이션이 반복될때의 처리 코드를 작성
+//                }
+//            })
+//        }
+
+        AnimatorInflater.loadAnimator(this, R.animator.property_animation).apply {
+            addListener(object: AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator?) {
+                    super.onAnimationEnd(animation)
+                    start()
+                }
+            })
+            setTarget(binding.imageView)
+            start()
+        }
+
+        AnimatorInflater.loadAnimator(this, R.animator.color_anim).apply {
+            val colorAnimator = this@apply as? ObjectAnimator
+            colorAnimator?.apply {
+                setEvaluator(ArgbEvaluator())
+                target = window.decorView.findViewById(android.R.id.content)
+                start()
+            }
+        }
+
     }
 
     // 펀치력 측정이 완료된 경우 처리 함수
--- punchpower/app/src/main/java/com/kyonggi/punchpower/ResultActivity.kt
+++ punchpower/app/src/main/java/com/kyonggi/punchpower/ResultActivity.kt
@@ -1,10 +1,22 @@
 package com.kyonggi.punchpower
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.games.Games
 import com.kyonggi.punchpower.databinding.ActivityResultBinding
 
 class ResultActivity : AppCompatActivity() {
+    val RC_SIGN_IN = 9000
+    val RC_LEADERBOARD_UI = 9004
+    val signingClient: GoogleSignInClient by lazy {
+        GoogleSignIn.getClient(this, GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN)
+    }
     val binding by lazy { ActivityResultBinding.inflate(layoutInflater) }
 
     // 펀치력을 저장하는 변수. 사용하는 시점에 초기화되도록 lazy
@@ -17,10 +29,56 @@ class ResultActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        binding.scoreLabel.text = "${String.format("%.0f", power)} 점"
+        with(binding) {
+            scoreLabel.text = "${String.format("%.0f", power)} 점"
 
-        binding.restartButton.setOnClickListener {
-            finish()
+            restartButton.setOnClickListener {
+                finish()
+            }
+
+            rankingButton.setOnClickListener {
+                // 이전에 구글 인증을 받은적이 있는지 체크. 널이면 아직 인증받은적 없음
+                if (GoogleSignIn.getLastSignedInAccount(this@ResultActivity) == null) {
+                    // sign in 필요
+                    startActivityForResult(signingClient.signInIntent, RC_SIGN_IN)
+                } else {
+                    uploadScore()
+                }
+            }
+        }
+    }
+
+    private fun uploadScore() {
+        // 인증된 유저 객체 가져옴
+        var user = GoogleSignIn.getLastSignedInAccount(this)
+        user?.let {
+            val leaderboard = Games.getLeaderboardsClient(this, user)
+            // 리더보드 객체에 점수를 즉시 올림. 성공시 컬백에서 리더보드 화면으로 이동
+            leaderboard.submitScoreImmediate(getString(R.string.leaderboard_id), power.toLong())
+                .addOnSuccessListener {
+                    leaderboard.getLeaderboardIntent(getString(R.string.leaderboard_id))
+                        .addOnSuccessListener { intent ->
+                            startActivityForResult(intent, RC_LEADERBOARD_UI)
+                        }
+                }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            val result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            result?.let { result ->
+                if (result.isSuccess) {
+                    uploadScore()
+                } else {
+                    var message = result.status.statusMessage
+                    if (message == null || message.isEmpty()) {
+                        message = "로그인 오류!"
+                    }
+                    Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
 }
