164ef647f652d3bac9b3976e9f5e263107165dcb
Joonhoo <mnc5@naver.com>
2021-09-20 21:43:42 +0900

    210920

---
+++ app/src/main/java/com/kyonggi/seoultoliet/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.kyonggi.seoultoliet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kyonggi.seoultoliet.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
